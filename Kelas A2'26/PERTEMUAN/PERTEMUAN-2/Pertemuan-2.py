849abad8852b9065d748269d3790ffc1570ff533
AhmadKhairilHidayat <khairilicpaser@gmail.com>
2026-09-19 08:09:30 +0800

    2609106024_AHMAD KHAIRIL HIDAYAT_POSTTEST 2_A2'26.py

---
+++ Kelas A2'26/PERTEMUAN/PERTEMUAN-2/Pertemuan-2.py
@@ -0,0 +1,26 @@
+#a = 5
+#b = 10.5
+#c = a + b
+#print(c)
+
+#nama = "Larry"
+#hobi = "ngoding"
+#print("Nama Saya " + nama)
+#print("dan Hobi Saya adalah " + hobi)
+
+#teks = "Rahmat Flowchart"
+
+#menampilkan index ke-3
+#print(teks[3])
+#menampilkan index ke-3 hingga ke-6
+#print(teks[3:6])
+#menampilkan seluruhnya, tetapi dimulai dari index ke-8
+#print(teks[8:])
+#menampilkan hanya sampai index ke-6
+#print(teks[:6])
+
+#a = "20"
+#print(type(a))
+
+#nama = input("utang plus utang = ")
+#print (nama)
